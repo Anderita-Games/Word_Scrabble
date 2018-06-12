8a3348f47813d47785b660e609a7f32e69ded6ec
Andrew Friedman <andrewfriedman01@gmail.com>
2018-06-11 23:32:21 -0400

    Added App
    This is the first version of Word Scrabble that GitHub has ever seen!

---
+++ Assets/Code/Start_UI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_UI : MonoBehaviour {
+	public UnityEngine.UI.Text Highscore;
+
+	// Use this for initialization
+	void Start () {
+		Highscore.text = " Highscore: " + PlayerPrefs.GetInt("Highscore").ToString();
+	}
+
+	public void Start_Game () {
+		Application.LoadLevel("Game");
+	}
+}
